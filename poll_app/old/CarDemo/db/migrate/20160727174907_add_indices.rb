0c6a64942197a44291a04bb6095404659b017d71
eliasylee <eliasylee@gmail.com>
2016-07-28 15:14:39 -0700

    Removed old projects

--- poll_app/old/CarDemo/db/migrate/20160727174907_add_indices.rb
+++
@@ -1,7 +0,0 @@
-class AddIndices < ActiveRecord::Migration
-  def change
-    add_index :cars, :owner_id
-    add_index :rentals, :renter_id
-    add_index :rentals, [:renter_id, :car_id]
-  end
-end
