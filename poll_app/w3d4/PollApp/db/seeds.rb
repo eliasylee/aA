a09b36470091f3a17c21c773ea6363bd26a071c0
eliasylee <eliasylee@gmail.com>
2016-07-28 11:24:55 -0700

    Finished seeding, tested associations

--- poll_app/w3d4/PollApp/db/seeds.rb
+++ poll_app/w3d4/PollApp/db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(user_name: 'Eric')
+User.create(user_name: 'Eli')
+User.create(user_name: 'Bob')
+
+Poll.create(title: 'Climbing Poll', author_id: 1)
+Poll.create(title: 'Computer Poll', author_id: 2)
+
+Question.create(text: 'What day?', poll_id: 1)
+Question.create(text: 'Where?', poll_id: 1)
+Question.create(text: 'What OS?', poll_id: 2)
+
+AnswerChoice.create(text: 'Saturday', question_id: 1)
+AnswerChoice.create(text: 'Sunday', question_id: 1)
+AnswerChoice.create(text: 'Dogpatch', question_id: 2)
+AnswerChoice.create(text: 'Mission Cliffs', question_id: 2)
+AnswerChoice.create(text: 'Linux', question_id: 3)
+AnswerChoice.create(text: 'OSX', question_id: 3)
+
+Response.create(user_id: 1, answer_choice_id: 1)
+Response.create(user_id: 1, answer_choice_id: 3)
+Response.create(user_id: 1, answer_choice_id: 5)
+
+Response.create(user_id: 2, answer_choice_id: 1)
+Response.create(user_id: 2, answer_choice_id: 4)
+Response.create(user_id: 2, answer_choice_id: 5)
+
+Response.create(user_id: 3, answer_choice_id: 2)
+Response.create(user_id: 3, answer_choice_id: 3)
+Response.create(user_id: 3, answer_choice_id: 6)
