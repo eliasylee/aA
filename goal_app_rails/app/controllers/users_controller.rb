14bda6dfb4b80054d9c70d0fabb4c3bb50f75282
eliasylee <eliasylee@gmail.com>
2016-08-08 11:54:10 -0700

    Implemented users controller and users controller spec, sign-up page functional

--- goal_app_rails/app/controllers/users_controller.rb
+++ goal_app_rails/app/controllers/users_controller.rb
@@ -5,24 +5,22 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    
+
     if @user.save
       sign_in(@user)
-      redirect_to :index
+      redirect_to user_url(@user)
     else
-      flash.now[:errors] = @users.errors.full_messages
+      flash.now[:errors] = @user.errors.full_messages
       render :new
     end
   end
 
-  def destroy
+  def show
     @user = User.find(params[:id])
-    @user.destroy
-    @user.redirect_to :new
   end
 
   private
   def user_params
-    params.require(:user).permit(:username)
+    params.require(:user).permit(:username, :password)
   end
 end
